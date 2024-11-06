f17b6ef4b5b8e60335280e38f8d9261523f53340
Alperen <alperenakkoz37@gmail.com>
2024-11-06 21:27:28 +0300

    Wait and Notify example added

---
+++ src/main/java/org/Alperen/threadSynchronisation/WaitAndNotify.java
@@ -0,0 +1,40 @@
+package org.Alperen.threadSynchronisation;
+
+public class WaitAndNotify {
+    private static final Object LOCK = new Object();
+    public static void main(String[] args) {
+        Thread one = new Thread(()->{
+            try {
+                one();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread two = new Thread(()->{
+            try {
+                two();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        one.start();
+        two.start();
+    }
+
+    private static void one() throws InterruptedException{
+        synchronized (LOCK){
+            System.out.println("Entered method one"); //first
+            LOCK.wait();
+            System.out.println("Back again. Method one"); //fourth
+        }
+    }
+
+    private static void two() throws InterruptedException{
+        synchronized (LOCK){
+            System.out.println("Entered method two"); //second
+            LOCK.notify();
+            System.out.println("After notify still two"); //third
+        }
+    }
+}
