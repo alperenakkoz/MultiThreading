b6f335bfc97ee35e3ab5f783dedab67c334ba29e
Alperen <alperenakkoz37@gmail.com>
2024-11-08 17:43:23 +0300

    ForkJoinPool (RecursiveAction) example added

---
+++ src/main/java/org/Alperen/forkJoin/WorkLoadSplitter.java
@@ -0,0 +1,42 @@
+package org.Alperen.forkJoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+
+//A recursive resultless ForkJoinTask.
+//mimic load balance
+public class WorkLoadSplitter extends RecursiveAction {
+
+    private final long workLoad;
+
+    public WorkLoadSplitter(long workLoad) {
+        this.workLoad = workLoad;
+    }
+
+    @Override
+    protected void compute() {
+        if (this.workLoad > 16) {
+            System.out.println("Work Load too big, thus splitting : " + this.workLoad);
+            long firstWorkLoad = this.workLoad/2;
+            long secondWorkLoad = this.workLoad - firstWorkLoad;
+
+            WorkLoadSplitter firstSplit = new WorkLoadSplitter(firstWorkLoad);
+            WorkLoadSplitter secondSplit = new WorkLoadSplitter(secondWorkLoad);
+
+            firstSplit.fork();
+            secondSplit.fork();
+        } else {
+            System.out.println("Work Load within limits! Done by: " + Thread.currentThread().getName());
+        }
+    }
+}
+
+class WorkLoadSplitDemo {
+    public static void main(String[] args) {
+        try (ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors())) {
+            WorkLoadSplitter splitter = new WorkLoadSplitter(128);
+            pool.invoke(splitter);
+        }
+    }
+}
